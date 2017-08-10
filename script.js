043769808dc340e2861543ad678b7946bf98ce71
osman abate <abateo@uw.edu>
2017-08-09 23:11:25 -0700

    added random color functionality

--- script.js
+++ script.js
@@ -1,10 +1,41 @@
 var penColour = 'black';
 
-function setPenColour(pen)
-{
-	penColour = pen
+window.addEventListener("load", function(event) {
+    console.log("All resources finished loading!");
+
+var pixel = document.getElementById('art');
+
+for(var i = 0; i < 500; i++) {
+	pixel.innerHTML += "<div class='pixel' onclick='setPixelColour(this)'></div>";
+
+	if(i % 100 == 0) {
+	}
+	
 }
+
+
+var divArray = document.getElementById('palette');
+
+for(var i = 0; i < 8; i++) {
+	var color = Math.random() * (10,25) + 10;
+	var r = Math.floor(Math.random()*256);          // Random between 0-255
+var g = Math.floor(Math.random()*256);          // Random between 0-255
+var b = Math.floor(Math.random()*256);          // Random between 0-255
+var rgb = 'rgb(' + r + ',' + g + ',' + b + ')'; // Collect all to a string
+	var color = divArray.querySelectorAll('div')[i];
+	color.style.backgroundColor = rgb;
+	color.setAttribute('data-color', rgb)
+}
+
+});
+
+
+function setPenColour(event)
+{
+  penColour = event.target.getAttribute('data-color');
+}
+
 function setPixelColour(pixel)
 {
-	pixel.style.backgroundColor = penColour;
+  pixel.style.backgroundColor = penColour;
 }
